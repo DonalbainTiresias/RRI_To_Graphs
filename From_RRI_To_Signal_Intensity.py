659fd286433ba37ea91f9090144b167ed185ef20
s267636 <s267636@SOW761624C>
2019-05-02 11:22:26 +0100

    Updated From Within Software

--- From_RRI_To_Signal_Intensity.py
+++ From_RRI_To_Signal_Intensity.py
@@ -19,9 +19,9 @@ print str(datetime.datetime.now())
 
 #Test Test Test
 #%%
-drive='z'
-book='1'
-page='199'
+drive='c'
+book='2'
+page='45b'
 
 #This sets the range of the data to be looked at for finding the peak of the Lorentzian fit.
 
